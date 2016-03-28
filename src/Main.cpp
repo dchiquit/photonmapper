ccd821f01119351ce915d20722f116deab82bcb7
avitkus <avitkus7@gmail.com>
2016-03-28 18:24:56 -0400

    Doesn't segfault from filling stack before running anymore. Turns out a 500*500*3 element float array is really big.

--- src/Main.cpp
+++ src/Main.cpp
@@ -18,12 +18,12 @@ int main(int argc, char** argv) {
     const int width = 500;
     const int height = width;
     std::cout << "test" << std::endl;
-    std::array<float, height*width*3> buffer;
-    buffer.fill(0.0f);
+    auto bufPtr = std::make_shared<float*>(new float[width*height*3]);
+    float* buffer = *bufPtr;
     std::cout << "test2" << std::endl;
-//    for (int i = 0; i < height * width * 3; i++) {
-//        buffer[i] = 0.0f;
-//    }
+    for (int i = 0; i < height * width * 3; i++) {
+        buffer[i] = 0.0f;
+    }
     
     auto sphere = std::make_shared<Sphere>(Vector3D{0, 0, -7}, 2);
     Scene scene{sphere};
@@ -31,7 +31,7 @@ int main(int argc, char** argv) {
     scene.addObject(std::make_shared<Sphere>(Vector3D{0, 4, -7}, 2));
     scene.addLight(std::make_shared<PointLight>(Vector3D{-4, 4, 3}));
 
-    render(scene, buffer.data(), width, height);
+    render(scene, buffer, width, height);
 
-    OpenGL_display(&argc, argv, buffer.data(), width, height);
+    OpenGL_display(&argc, argv, buffer, width, height);
 };
